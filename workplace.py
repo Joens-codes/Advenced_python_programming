bdd690b248ca955bad3c94a5c7b8f2ed763fc9f9
Hyomin Jeon <xkfmqhtkdn@hanmail.net>
2025-05-21 09:52:45 +0900

    AI 모델링 작업공간

---
+++ workplace.py
@@ -0,0 +1,28 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "데이터 전처리 공간\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"데이터 전처리 공간\")"
+   ]
+  }
+ ],
+ "metadata": {
+  "language_info": {
+   "name": "python"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
