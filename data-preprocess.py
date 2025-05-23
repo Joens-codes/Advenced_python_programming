c29ce6e52c3c803f5268c9c0cd3b92ca840e4673
bangjeongwoo1 <bjwoo0625@gmail.com>
2025-05-23 19:51:52 +0900

    결측치 처리

--- data-preprocess.py
+++ data-preprocess.py
@@ -3,6 +3,17 @@ import pandas as pd
 
 def preprocess_ev_data(file_path): # EV 충전 패턴 데이터 전처리
     
+    """
+    지정한 컬럼(또는 전체)에서 결측치가 있는 행을 제거하는 함수
+
+    Parameters:
+    - file_path : str
+        EV 충전 패턴 데이터가 저장된 CSV 파일 경로
+    Returns:
+    - EV_df : pandas.DataFrame
+        전처리된 EV 충전 패턴 데이터프레임
+    """
+    
     df = pd.read_csv(file_path)
     
     columns_to_keep = [
@@ -50,7 +61,31 @@ def check_missing_values(df):
 
     return missing_df
 
+def remove_missing_values(df, columns=None):
+    """
+    지정한 컬럼(또는 전체)에서 결측치가 있는 행을 제거하는 함수
+
+    Parameters:
+    - df : pandas.DataFrame  
+        결측치를 제거할 데이터프레임  
+    - columns : list or None, default=None  
+        결측치를 확인할 컬럼 리스트. None이면 전체 컬럼에서 확인함.
+
+    Returns:
+    - df_clean : pandas.DataFrame  
+        결측치가 제거된 데이터프레임 복사본
+    """
+    if columns:
+        df_clean = df.dropna(subset=columns).copy()
+    else:
+        df_clean = df.dropna().copy()
+    
+    return df_clean
+
+
 if __name__ == "__main__":
     df = preprocess_ev_data('ev_charging_patterns.csv')
     print(df.head())
     print(check_missing_values(df))
+    df_remove = remove_missing_values(df)
+    print(check_missing_values(df_remove))
